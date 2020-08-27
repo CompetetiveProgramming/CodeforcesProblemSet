8cbfbc94940c44d9d4efe18f92f53aefcf4d9990
naphade21 <56242029+naphade21@users.noreply.github.com>
2020-08-27 20:53:51 +0530

    Added Linked List

---
+++ Data Structures/Linked List/Singly Linked list.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* Singly Linked List */
+typedef struct list{
+  int item;
+  struct list *next;
+}list;
+
+/* Searching a list */
+
+list *Search(list *l, int x){
+  if( l == NULL) return(NULL);
+  if(l->item == x){
+    return(l);
+  }
+  else{
+    return(Search(l->next, x));
+  }
+}
+
+/* Insertion */
+
+void InsertToList(list **l, int x){
+  list *p;
+
+  p = malloc(sizeof(list));
+  p->item = x;
+  p->next = *l;
+  *l = p;
+}
+
+/* Deletion from List */
+
+void DeleteElement(list *l, int x){
+
+}
